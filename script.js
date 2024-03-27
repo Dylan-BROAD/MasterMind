5406f83fa6f609c0752df7bb10cf199500c5f27e
Dylan Broad <dylan@Dylans-Air.home>
2024-03-27 22:32:05 +1100

    More CSS and HTML updates. Also using DOM manipulation.

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
-/*----- constants -----*/
+const main_display = document.querySelector(".content");
+const submit_button = document.getElementById("submit");
+const reset_button = document.getElementById("reset");
+const clear_button = document.getElementById("clear");
 
-/*----- state variables -----*/
+let codeLength = 4;
+let trys = 10;
+let colors = ["green", "blue", "red", "purple", "yellow", "orange"];
 
-/*----- cached elements  -----*/
+let random_code = [];
+let attempt = 1;
 
-/*----- event listeners -----*/
+init();
 
-/*----- functions -----*/
+function init() {
+  // Reset random_code and attempt
+  random_code = [];
+  attempt = 1;
 
-function(){}
+  // Clear gameBoard and colorChoice
+  //main_display.innerHTML = " ";
+  //colorChoice.innerHTML = " ";
+
+  // Display board
+  for (let i = 1; i <= trys; i++) {
+    let div_attempt = document.createElement("div");
+    div_attempt.setAttribute("id", "attempt-" + i);
+    div_attempt.setAttribute("class", "attempt");
+    let div_hintColumn = document.createElement("div");
+    div_hintColumn.setAttribute("class", "hintColumn");
+    let div_guessColumn = document.createElement("div");
+    div_guessColumn.setAttribute("class", "guessColumn");
+
+    for (let j = 1; j <= codeLength; j++) {
+      let div_tr = document.createElement("div");
+      let div_gr = document.createElement("div");
+      div_hintColumn.append(div_tr);
+      div_guessColumn.append(div_gr); // Append div_gr to div_guessColumn
+    }
+
+    // Append both rows to the attempt div
+    div_attempt.append(div_hintColumn);
+    div_attempt.append(div_guessColumn);
+
+    // Prepend the attempt div to content_display
+    main_display.append(div_attempt);
+  }
+}
