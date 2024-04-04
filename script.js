28a36185837d851c77f4fce588aa73894bb0ce64
Dylan Broad <dylan@Dylans-Air.home>
2024-04-04 20:25:53 +1100

    WIP: re-adding modal JS

--- script.js
+++ script.js
@@ -196,32 +196,28 @@ function submitGuess() {
 }
 
 // Event listener for the submit button
-// submit_button.addEventListener("click", submitGuess);
-// // Event listener for the Reset button
-// reset_button.addEventListener("click", resetGame);
-// // Event listener for the Clear button
-// clear_button.addEventListener("click", clearChoices);
+submit_button.addEventListener("click", submitGuess);
+// Event listener for the Reset button
+reset_button.addEventListener("click", resetGame);
+// Event listener for the Clear button
+clear_button.addEventListener("click", clearChoices);
 
-// // Get the modal
-// var modal = document.getElementById("myModal");
+// Get the modal
+var modal = document.getElementById("myModal");
 
-// // Get the button that opens the modal
-// var btn = document.getElementById("info");
+// Get the button that opens the modal
+var btn = document.getElementById("info");
 
-// // Get the <span> element that closes the modal
-// var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
 
-// // When the user clicks on the button, open the modal
+// When the user clicks on the button, open the modal
 
-// function modal() {
-//   modal.style.display = "block";
-// }
+btn.onclick = function () {
+  modal.style.display = "block";
+};
 
-// btn.onclick = function () {
-//   modal.style.display = "block";
-// };
-
-// // When the user clicks on <span> (x), close the modal
-// span.onclick = function () {
-//   modal.style.display = "none";
-// };
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+};
